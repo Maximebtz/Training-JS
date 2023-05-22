a54f42ed0cd8cc5a337516354d11551ea4767a39
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-05-22 17:41:40 +0200

    Update

--- Exo-3/main.js
+++ Exo-3/main.js
@@ -1,18 +1,46 @@
 
+const box = document.createElement('div');
+box.classList.add('square');
 
-const box = document.createElement('button');
-box.classList.add('box');
-
-
+const btn = document.querySelector("button")
 const board = document.querySelector('#board');
 
-board.appendChild(box);
-
-box.innerText = ("TEXT")
 
 
-box.addEventListener('click', function() {
-    alert("Class : box\n - width: 200px\n - height: 200px\n - background-color: darkcyan\n - font-size: 3em\n - font-weight: 600\n - color: ghostwhite")
-})
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+let nb = 1;
+
+function addSquare(){
+  
+  for(let i = 1; i <= 1; i++){
+      
+      const newbox = box.cloneNode(); 
+      board.appendChild(newbox);
+      newbox.style.background = getRandomColor();
+  }
+}
+
+function delSquare(){
+    const squares = document.querySelectorAll('.square');
+    if (squares.length > 0) {
+      const lastSquare = squares[squares.length - 1];
+      board.removeChild(lastSquare);
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "ArrowDown") {
+      addSquare();
+    } else if (event.key === "ArrowUp") {
+      delSquare()
+    }
+});
 
